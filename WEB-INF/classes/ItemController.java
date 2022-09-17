3d4321666b52ce51bab3f9f48cfeb1bf91a050a1
James Worcester <40445386+jamesworcester@users.noreply.github.com>
2022-09-17 15:54:28 +1000

    Completed Task 2

--- WEB-INF/classes/ItemController.java
+++ WEB-INF/classes/ItemController.java
@@ -16,7 +16,8 @@ public class ItemController {
 
     public List<Item> get() throws Exception {
         //TODO 13: HINT: a relevant call to a warehouseDSC method
-        return null; // this method should not return null
+        //return null; // this method should not return null
+        return warehouseDSC.getAllItems();
     }
     
     public List<Item> getAllExpiredItems() throws Exception {
@@ -27,18 +28,16 @@ public class ItemController {
     public static void main(String [] args) throws Exception {
         // CONSIDER testing each of the above methods here
         // NOTE: this is not a required task, but will help you test your Task 2 requirements
-        try {
+        //try {
             // CHANGE the username and password to match yours
             // CHANGE the first param to your database host if you are not using latcs7
-            ItemController ic = new ItemController("latcs7.cs.latrobe.edu.au:3306", "your-mysql-username", "your-mysql-password");
-            /* UNCOMMENT the following and add the relevant parameters/arguments to do your testing
-            System.out.println(gc.get());
+            //ItemController ic = new ItemController("localhost:3306/warehousedb", "root", "password");
+            // UNCOMMENT the following and add the relevant parameters/arguments to do your testing
+            //System.out.println(ic.get());
 
-            */
-
-        } catch (Exception exp) {
-            exp.printStackTrace();
-        }
+        //} catch (Exception exp) {
+        //    exp.printStackTrace();
+        //}
 
     }
 }
